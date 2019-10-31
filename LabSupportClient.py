c57b28a301e8ea2054ee45acbf62010747fcb0cb
Avinoam <avinoam@hershler.com>
2019-10-31 08:33:43 +0200

    Added update attempt at startup

--- LabSupportClient.py
+++ LabSupportClient.py
@@ -1,4 +1,7 @@
 from app.src import GUI
+from app.src.updates import updater
 
 if __name__ == '__main__':
+    if updater.do_update():
+        print("Client updated, please restart.")
     gui = GUI.Gui()
