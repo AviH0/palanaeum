1260aa6ae1102c22ec0dee2d58225baa14903da6
Avinoam <avinoam@hershler.com>
2019-12-14 22:15:25 +0200

    Update with no restart

--- LabSupportClient.py
+++ LabSupportClient.py
@@ -1,6 +1,5 @@
 import sys
 
-from app.src import GUI
 from app.src.updates import updater
 
 if __name__ == '__main__':
@@ -8,6 +7,6 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         updates_disabled = sys.argv[1] == "--no-updates"
     if not updates_disabled and updater.do_update():
-        print("Client updated, please restart.")
-        exit(0)
+        print("Client updated!")
+    from app.src import GUI
     gui = GUI.Gui()
